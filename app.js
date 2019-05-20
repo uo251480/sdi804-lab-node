f333a7c50483c4c03911dc74ad0be02afab763e1
David <uo251480@uniovi.es>
2019-05-20 18:23:31 +0200

    SDI-NodeJS-10.1- SW API REST Canción

--- app.js
+++ app.js
@@ -108,6 +108,8 @@ app.set('crypto',crypto);
 //Rutas/controladores por lógica
 require("./routes/rusuarios.js")(app, swig, gestorBD); // (app, param1, param2, etc.)
 require("./routes/rcanciones.js")(app, swig, gestorBD); // (app, param1, param2, etc.)
+require("./routes/rapicanciones.js")(app, gestorBD);
+
 
 app.get('/', function (req, res) {
     res.redirect('/tienda');
