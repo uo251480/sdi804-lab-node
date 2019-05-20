9d383bdf490c89f3b4c8c21eca09cb9972b1ff57
David <uo251480@uniovi.es>
2019-05-20 17:55:37 +0200

    SDI-NodeJS-9.3- Redirección de peticiones

--- app.js
+++ app.js
@@ -90,6 +90,10 @@ app.set('crypto',crypto);
 require("./routes/rusuarios.js")(app, swig, gestorBD); // (app, param1, param2, etc.)
 require("./routes/rcanciones.js")(app, swig, gestorBD); // (app, param1, param2, etc.)
 
+app.get('/', function (req, res) {
+    res.redirect('/tienda');
+})
+
 
 // lanzar el servidor
 app.listen(app.get('port'), function() {
--- routes/rusuarios.js
+++ routes/rusuarios.js
@@ -20,7 +20,7 @@ module.exports = function(app, swig, gestorBD) {
             if (id == null){
                 res.send("Error al insertar ");
             } else {
-                res.send('Usuario Insertado ' + id);
+                res.redirect("/identificarse");
             }
         });
 
@@ -44,7 +44,7 @@ module.exports = function(app, swig, gestorBD) {
                 res.send("No identificado: ");
             } else {
                 req.session.usuario = usuarios[0].email;
-                res.send("identificado");
+                res.redirect("/publicaciones");
             }
         });
 
