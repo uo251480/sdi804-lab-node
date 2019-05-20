17becddc4d9a661adc977742fc4be0b82d906e25
David <uo251480@uniovi.es>
2019-05-20 16:20:45 +0200

    SDI-NodeJS-8.3 - Arquitectura para el acceso a datos

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+var gestorBD = require("./modules/gestorBD.js");
+gestorBD.init(app,mongo);
+
+
 app.use(express.static('public'));
 
 // Variables
@@ -15,8 +19,8 @@ app.set('port', 8081);
 app.set('db','mongodb://admin:sdi@tiendamusica-shard-00-00-dwbpp.mongodb.net:27017,tiendamusica-shard-00-01-dwbpp.mongodb.net:27017,tiendamusica-shard-00-02-dwbpp.mongodb.net:27017/test?ssl=true&replicaSet=tiendamusica-shard-0&authSource=admin&retryWrites=true');
 
 //Rutas/controladores por lógica
-require("./routes/rusuarios.js")(app, swig); // (app, param1, param2, etc.)
-require("./routes/rcanciones.js")(app, swig, mongo); // (app, param1, param2, etc.)
+require("./routes/rusuarios.js")(app, swig, gestorBD); // (app, param1, param2, etc.)
+require("./routes/rcanciones.js")(app, swig, gestorBD); // (app, param1, param2, etc.)
 
 
 // lanzar el servidor
--- routes/rcanciones.js
+++ routes/rcanciones.js
@@ -1,4 +1,4 @@
-module.exports = function(app, swig, mongo) {
+module.exports = function(app, swig, gestorBD) {
 
     app.get('/canciones/agregar', function (req, res) {
         var respuesta = swig.renderFile('views/bagregar.html', {
@@ -64,21 +64,15 @@ module.exports = function(app, swig, mongo) {
             precio : req.body.precio
         }
         // Conectarse
-        mongo.MongoClient.connect(app.get('db'), function(err, db) {
-            if (err) {
-                res.send("Error de conexión: " + err);
+        // Conectarse
+        gestorBD.insertarCancion(cancion, function(id){
+            if (id == null) {
+                res.send("Error al insertar canción");
             } else {
-                var collection = db.collection('canciones');
-                collection.insert(cancion, function(err, result) {
-                    if (err) {
-                        res.send("Error al insertar " + err);
-                    } else {
-                        res.send("Agregada id: "+ result.ops[0]._id);
-                    }
-                    db.close();
-                });
+                res.send("Agregada la canción ID: " + id);
             }
         });
+
 
     });
 
